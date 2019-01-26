244cccdda6a1571917e44e2d51c78c5fc34135f0
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2019-01-26 18:20:19 +0100

    [ADD] Player camera

---
+++ Rythome/Assets/Scripts/Enhance/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+	[SerializeField]
+	private Transform Target = null;
+	private Vector3 Offset = Vector3.back * 10f;
+
+	public float Speed = 1f;
+
+	private void Awake()
+	{
+		if (!Target)
+			return;
+
+		Offset = transform.position - Target.position;
+	}
+
+	private void LateUpdate()
+	{
+		if (!Target)
+			return;
+
+		transform.position = Vector3.Lerp(transform.position, Target.position + Offset, Speed * Time.deltaTime);
+	}
+}

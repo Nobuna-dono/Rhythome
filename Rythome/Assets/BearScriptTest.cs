3af3eb73ab4ce7a91c078074be69d137d6abf36f
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2019-01-26 15:04:05 +0100

    [ADD] Test Bear animator

---
+++ Rythome/Assets/BearScriptTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearScriptTest : MonoBehaviour
+{
+	public Animator animator = null;
+
+	private void Update()
+	{
+		if (animator)
+			animator.SetBool("IsRunning", Input.GetKey(KeyCode.Z));
+	}
+}

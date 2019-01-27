d2d6ed9b06f6bfdda607aee8507fc6a8e7403848
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2019-01-27 04:32:14 +0100

    [ADD] Shaking script

---
+++ Rythome/Assets/Scripts/Enhance/ShakingObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShakingObject : MonoBehaviour
+{
+	public float Speed = 1f;
+
+	[SerializeField]
+	private Vector2 ShakeIntensity = Vector2.one;
+	private Vector3 origin = Vector3.zero;
+
+	private void OnEnable()
+	{
+		origin = transform.position;
+	}
+
+	private void OnDisable()
+	{
+		transform.position = origin;
+	}
+
+	private void Update()
+	{
+		Vector2 offset = Mathf.Sin(Time.time * Speed) * ShakeIntensity;
+		transform.position = origin + new Vector3(offset.x, offset.y, 0f);
+	}
+}

5ea11b52913f689d751c8ee44f00b1e805f48089
Morgan Hoarau <morgansb.hoarau@gmail.com>
2019-01-27 10:08:29 +0100

    [UPDATE] TAMAYO

---
+++ Rythome/Assets/Scripts/AnimationBegin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhythome.Gameplay
+{
+
+    public class AnimationBegin : MonoBehaviour
+    {
+        [SerializeField]
+        private StateMachineSyncDataOfInt State;
+
+        [SerializeField]
+        Animator m_Animator;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetState(int _index)
+        {
+            if(m_Animator)
+               State.SetData(_index, m_Animator);
+        }
+    }
+}

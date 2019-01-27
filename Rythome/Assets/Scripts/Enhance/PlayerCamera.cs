26df3148e53f07c371d87ccfaa72b48f19a6f121
Morgan Hoarau <morgansb.hoarau@gmail.com>
2019-01-27 11:07:33 +0100

    [UPDATE] Camera

--- Rythome/Assets/Scripts/Enhance/PlayerCamera.cs
+++ Rythome/Assets/Scripts/Enhance/PlayerCamera.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
+using Rhythome.Core;
+
 public class PlayerCamera : MonoBehaviour
 {
 	[SerializeField]
 	private Transform Target = null;
 	private Vector3 Offset = Vector3.back * 10f;
 
-	public float Speed = 1f;
+	public float SpeedFactor = 1f;
+	private float Speed = 1f;
 
-	private void Awake()
+    private void Awake()
 	{
 		if (!Target)
 			return;
@@ -16,11 +19,21 @@ public class PlayerCamera : MonoBehaviour
 		Offset = transform.position - Target.position;
 	}
 
-	private void LateUpdate()
+    private void Start()
+    {
+        ServiceSupervisor.Instance.Rythm.OnTimeScaleUpdate += UpdateSpeed;
+    }
+
+    private void LateUpdate()
 	{
 		if (!Target)
 			return;
 
 		transform.position = Vector3.Lerp(transform.position, Target.position + Offset, Speed * Time.deltaTime);
 	}
+
+    public void UpdateSpeed(float _newSpeed)
+    {
+        Speed = _newSpeed * SpeedFactor;
+    }
 }
